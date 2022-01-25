ecb3ddbc478565e35f91c1f98cb42b3a30ea169e
Stefan Schneider-Kennedy <code@stefansk.name>
2022-01-25 14:09:57 +1100

    Add simple Talon status overlay

---
+++ subtitles.py
@@ -0,0 +1,54 @@
+"""
+Show a small panel in my status tray containing the last utterance, current mode indicator, and time
+since last utterance
+"""
+
+import math
+import datetime
+from talon import canvas, ui, speech_system, scope, cron
+
+
+last_phrase = ""
+last_phrase_time = None
+
+def calculate_last_phrase_time():
+    if last_phrase_time is None:
+        return ""
+
+    delta = (datetime.datetime.now() - last_phrase_time).total_seconds()
+    opts = [("h", 3600), ("m", 60), ("s", 1)]
+    for marker, divisor in opts:
+        if delta >= divisor:
+            return f"{math.floor(delta / divisor)}{marker}".rjust(3, " ")
+
+    return " 1s"
+
+def draw(can):
+    left = 150
+    top = rect.height
+
+    mode = "DM" if "dictation" in scope.get("mode") else "CM"
+    time = calculate_last_phrase_time()
+    label = f"{mode} {time} {last_phrase}"
+
+    can.paint.typeface = "monospace"
+    can.paint.color = "000000"
+    can.draw_text(label, left+2, top - 3)
+    can.paint.color = "ffffff"
+    can.draw_text(label, left, top - 5)
+
+def post_phrase(phrase):
+    global last_phrase, last_phrase_time
+
+    last_phrase = " ".join(phrase["text"])
+    last_phrase_time = datetime.datetime.now()
+    can.freeze()
+
+rect = ui.main_screen().rect
+can = canvas.Canvas.from_rect(rect)
+can.register("draw", draw)
+can.freeze()
+
+speech_system.register("post:phrase", post_phrase)
+scope.register("main.mode", can.freeze)
+cron.interval("10s", can.freeze)
